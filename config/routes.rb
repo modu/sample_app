a7641f22a1d26d8ab8b5c975781c7932416d60c8
Varun <varun@Kishan-Patels.local>
2012-01-06 17:00:40 +0530

    Added a title helper and stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
+  get "pages/help"  
 
+  get "pages/home"
   get "pages/contact"
   get "pages/about"
+  
+    
+  match '/signup',  :to => 'users#new'
+  match '/help' ,   :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
